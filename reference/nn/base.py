3cb52641a3dd940584d5913b1aa5cf139a164739
Ethan <luethan2025@outlook.com>
2024-07-07 11:01:12 -0700

    implemented Module

--- reference/nn/base.py
+++ reference/nn/base.py
@@ -13,3 +13,44 @@ class Parameter:
   def __init__(self, value):
     self.value = value
     self.grad = None
+
+class Module:
+  """Base class for network layers and activation functions.
+
+  Attributes
+  ----------
+  self.trainable_parameters : Parameter[]
+    List of parameters that can be trained in this module.
+  """
+  def __init__(self):
+    self.trainable_parameters = []
+
+  def forward(self, x):
+    """Forward propagation.
+
+    Parameters
+    ----------
+    x : np.array
+      Input for this module.
+
+    Returns
+    -------
+    np.array
+      Output of this module.
+    """
+    raise NotImplementedError()
+  
+  def backward(self, grad):
+    """Backward propagation.
+
+    Parameters
+    ----------
+    grad : np.array
+      Gradient flowing backwards from the next module.
+
+    Returns
+    -------
+    np.array
+      Gradients for the inputs to this module.
+    """
+    raise NotImplementedError()
