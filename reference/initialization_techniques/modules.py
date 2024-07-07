33403860818bbd785f6e742594002bfa1c63f158
Ethan <luethan2025@outlook.com>
2024-07-07 11:04:05 -0700

    implemented He

--- reference/initialization_techniques/modules.py
+++ reference/initialization_techniques/modules.py
@@ -31,3 +31,31 @@ class Xavier(Technique):
     b = np.zeros(shape=(self.out_dim))
 
     return W, b
+
+class He(Technique):
+  """Uniform He initialization.
+
+  Parameters
+  ----------
+  in_dim : int
+    Length of input dimensions.
+  out_dim : int
+    Length of output dimensions.
+  """
+  def __init__(self, in_dim, out_dim):
+    super().__init__(in_dim, out_dim)
+
+  def initialize_weights_and_bias(self):
+    """Apply He initialization strategy.
+
+    Returns
+    -------
+    (np.array, np.array)
+      [0] Initialized weight.
+      [1] Initialized bias.
+    """
+    u = np.sqrt(6 / float(self.in_dim))
+    W = np.random.uniform(low=-u, high=u, size=(self.out_dim, self.in_dim))
+    b = np.zeros(shape=(self.out_dim))
+
+    return W, b
