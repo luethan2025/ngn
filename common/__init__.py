e5536717e47e5df5fa3cc7190dacd345f0567758
Ethan Lu <luethan2025@outlook.com>
2024-12-19 15:15:19 -0800

    exported categorical_cross_entropy

--- common/__init__.py
+++ common/__init__.py
@@ -2,9 +2,11 @@
 
 from .base import Parameter, Module
 from . import dataset, optim
+from .loss import categorical_cross_entropy
 from .trainer import train_model
 
 __all__ = [
   "Parameter", "Module", "dataset", "optim",
+  "categorical_cross_entropy",
   "train_model"
 ]
