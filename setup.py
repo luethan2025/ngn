c814102fd5775c2de8590333eaaef2c4f5cbc65e
Ethan Lu <luethan2025@outlook.com>
2024-12-19 18:49:29 -0800

    added baseline/ and numeric/ to setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from setuptools import setup, find_packages
 setup(
   name="ngn",
     version='0.1',
-    packages=find_packages(include=["common"]),
+    packages=find_packages(include=["common", "baseline", "numeric"]),
     package_dir={
-      "common": "common"
+      "common": "common",
+      "baseline": "baseline",
+      "numeric": "numeric"
   }
 )
