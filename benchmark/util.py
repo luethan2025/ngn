1da36b37861a3f3a72669fb59a1c9288b1002bd4
Ethan Lu <luethan2025@outlook.com>
2024-12-21 17:50:50 -0800

    added loss and accuracy grapher

---
+++ benchmark/util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_loss_and_acc(losses, accs, labels=None, figsize=(15,7)):
+  assert len(losses) == len(accs)
+  fig, axarr = plt.subplots(1, 2, figsize=figsize)
+  axarr = np.array(axarr).reshape(-1) 
+
+  ax = axarr[0]
+  for idx, loss in enumerate(losses):
+    ax.plot(loss, label=labels[idx] if labels else None)
+  if labels:
+    ax.legend()
+  ax.set_title("Loss over epochs")
+
+  ax = axarr[1]
+  for idx, acc in enumerate(accs):
+    ax.plot(acc, label=labels[idx] if labels else None)
+  if labels:
+    ax.legend()
+  ax.set_title("Accuracy over epochs")
+  plt.tight_layout()
+  plt.show()
